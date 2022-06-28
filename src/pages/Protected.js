9fafcb19007d74ec5f50b468e6674826a730365c
amschell <100097727+amschell@users.noreply.github.com>
2022-06-28 18:53:44 +0300

    removed some redudancy in the Order component

--- src/pages/Protected.js
+++ src/pages/Protected.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {Outlet, Navigate, useLocation} from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 
-import Order from './Order'
+
 
 const Protected = () => {
     const location= useLocation()
 const {auth}= useAuth()
   return (
     <>
-   <Order/>
+ 
   {
 auth?.user?<Outlet/>:<Navigate to ="/login" state={{from:location}} replace/>
   }
