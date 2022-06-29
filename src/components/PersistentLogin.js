239559c2d798a8b2af3e4712b77ad5d9d4e5cb98
amschell <100097727+amschell@users.noreply.github.com>
2022-06-29 22:08:23 +0300

    added loading component

--- src/components/PersistentLogin.js
+++ src/components/PersistentLogin.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from '../hooks/useRefreshToken';
 import {useAuth} from '../hooks/useAuth';
+import Loader from "./Loader";
 
 const PersistentLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -40,7 +41,7 @@ const PersistentLogin = () => {
             {!persist
                 ? <Outlet />
                 : isLoading
-                    ? <p>Loading...</p>
+                    ? <Loader/>
                     : <Outlet />
             }
         </>
