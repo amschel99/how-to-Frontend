627f5d50266ef95d8438b6d21870cda6d08fa145
amschell <100097727+amschell@users.noreply.github.com>
2022-07-03 00:36:10 +0300

    added the products

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import axios from "axios"
+import { useQuery, useQueryClient } from 'react-query'
+import {motion} from "framer-motion"
+import "./css/Main.css"
+
 
 const Main = () => {
+  
+const fetchProducts= async ()=>{
+const response= await axios.get("http://localhost:5000")
+console.log(response)
+return await response.data
+}
+const{isLoading, isError,data}= useQuery("products", fetchProducts)
+if(isLoading){
+  return <>
+  <h5>loading ... Be patient</h5>
+  </>
+}
+if(isError){
+  return <>
+
+<h5>An error occured, our engineers are working on it</h5>
+</>
+
+}
+
+
   return (
-    <div>OOPS, NO Products TO DISPLAY</div>
+    <div className='caraousel'>
+ 
+
+       {data.map((product)=>{
+        return <div className='product' key={product.image} >
+        
+          <img  src={product.image} alt="product"/>
+          <h5>{product.name.substring(0,20)}</h5>
+          <h6>{product.price}</h6>
+       
+        </div>
+       })}
+         
+    </div>
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import {QueryClientProvider, QueryClient} from "react-query"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient= new QueryClient()
 root.render(
-
+   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <AuthProvider>
+     
  <App/>
+
     </AuthProvider>
    
     </BrowserRouter>
+     </QueryClientProvider>
   
 
  
